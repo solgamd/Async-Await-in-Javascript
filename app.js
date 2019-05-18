dc5906ca9f876a7ede004ca750d81ff4bf3913e6
solgamd <solgamd@gmail.com>
2019-05-18 17:32:31 +0200

    Finished Async/Await section. Lab complete.

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-slowMath.add(6, 2)
-.then((result) => {
-    console.log(result);
-    return slowMath.multiply(result, 2)
-}).then((result) => {
-    console.log(result);
-    return slowMath.divide(result, 2)
-}).then((result) => {
-    console.log(result);
-    return slowMath.subtract(result, 3)
-}).then((result) => {
-    console.log(result);
-    return slowMath.add(result, 98)
-}).then((result) => {
-    console.log(result);
-    return slowMath.remainder(result, 2);
-}).then((result) => {
-    console.log(result);
-    return slowMath.multiply(result, 50);
-}).then((result) => {
-    console.log(result);
-    return slowMath.remainder(result, 40);
-}).then((result) => {
-    console.log(result);
-    return slowMath.add(result, 32);
-}).then((result) => {
-    console.log(`The final result is ${result}`);
-}).catch((err) => {
-    console.log(err);
-})
+
+// PROMISE.THEN() CHAINING -------------------------------------->
+// slowMath.add(6, 2)
+// .then((result) => {
+//     console.log(result);
+//     return slowMath.multiply(result, 2)
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.divide(result, 2)
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.subtract(result, 3)
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.add(result, 98)
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.remainder(result, 2);
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.multiply(result, 50);
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.remainder(result, 40);
+// }).then((result) => {
+//     console.log(result);
+//     return slowMath.add(result, 32);
+// }).then((result) => {
+//     console.log(`The final result is ${result}`);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+// ASYNC/AWAIT -------------------------------------------------->
+
+const doMath = async () => {
+    try {    
+        let result = await slowMath.add(6, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 2);
+        console.log(result);
+        result = await slowMath.divide(result, 4);
+        console.log(result);
+        result = await slowMath.subtract(result, 3);
+        console.log(result);
+        result = await slowMath.add(result, 98);
+        console.log(result);
+        result = await slowMath.remainder(result, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 50);
+        console.log(result);
+        result = await slowMath.remainder(result, 40);
+        console.log(result);
+        result = await slowMath.add(result, 32);
+        console.log(`The final result is ${result}.`);
+    } catch (err) {
+        console.log(err);
+    }
+}
+doMath();
